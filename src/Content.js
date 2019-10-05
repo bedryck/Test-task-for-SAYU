4d6c10df91c4900a02c93cc9208cec22adbc14dd
Рома Коваль <romariooo27@gmail.com>
2019-10-05 12:08:33 +0300

    save time to localStorege

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Clock from './Clock';
 
 const Content = () => {
     const date = `${new Date().getMonth() + 1}/${new Date().getDate()}/${new Date().getFullYear()} `;// the date is now for future transform input[time]
     const [time, setTime] = useState(''); // input time
     const changedDate = time ? new Date(date + time).getTime() : Date.now(); // transform input
+
+    const _onSetTime = (e) => {
+        setTime(e.target.value);
+        localStorage.setItem('time', e.target.value);
+    };
+
+    useEffect(() => {
+        const storegeTime = localStorage.getItem('time');
+        if (storegeTime) setTime(storegeTime);
+    }, []);
+
     return (
         <div className="App-Content">
             <div className="clocks-container">
@@ -14,7 +25,7 @@ const Content = () => {
             </div>
             <div className="time-picker">
                 <p>Select time</p>
-                <input type="time" step="1" value={time} onChange={(e) => setTime(e.target.value)} />
+                <input type="time" step="1" value={time} onChange={_onSetTime} />
             </div>
         </div>
     );
