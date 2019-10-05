c605a4e42b42907b0932cbf9b162bdb45dc709e3
Рома Коваль <romariooo27@gmail.com>
2019-10-05 03:34:12 +0300

    added clock component

--- src/Clock.js
+++ src/Clock.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const Clock = ({ city }) => {
-    console.log(city);
+const Clock = ({ city, time }) => {
+    let INITIAL_TIME = time;
+    const [stateSeconds, setSeconds] = useState('00');
+    const [stateMinutes, setMinutes] = useState('00');
+    const [stateHoures, setHoures] = useState('00');
+
+    const clock = () => setInterval(() => {
+        const date = new Date(INITIAL_TIME);
+
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        hours = (hours < 10) ? `0${hours}` : hours;
+        minutes = (minutes < 10) ? `0${minutes}` : minutes;
+        seconds = (seconds < 10) ? `0${seconds}` : seconds;
+        setHoures(hours);
+        setMinutes(minutes);
+        setSeconds(seconds);
+        INITIAL_TIME += 1000;
+        console.log('clock');
+    }, 1000);
+
+    useEffect(() => {
+        clock();
+    }, []);
+
     return (
         <div className="clock">
             <div className="faceWatch">
                 <span>
-                    00:00:00
+
+                    {stateHoures}
+                    :
+                    {stateMinutes}
+                    :
+                    {stateSeconds}
                 </span>
             </div>
             <div className="city">
@@ -21,10 +50,12 @@ const Clock = ({ city }) => {
 
 Clock.propTypes = {
     city: PropTypes.string,
+    time: PropTypes.number,
 };
 
 Clock.defaultProps = {
     city: '',
+    time: Date.now(),
 };
 
 export default Clock;
--- src/Content.js
+++ src/Content.js
@@ -4,13 +4,13 @@ import Clock from './Clock';
 const Content = () => (
     <div className="App-Content">
         <div className="clocks-container">
-            <Clock city="London" />
-            <Clock city="Kiev" />
-            <Clock city="Rome" />
+            <Clock city="London" time={Date.now() - 2 * (60 * 60 * 1000)} />
+            <Clock city="Kiev" time={Date.now()} />
+            <Clock city="Rome" time={Date.now() - (60 * 60 * 1000)} />
         </div>
         <div className="time-picker">
             <p>Select time</p>
-            <input type="time" />
+            <input type="time" onChange={(e) => console.log(e.target.value)} />
         </div>
     </div>
 );
